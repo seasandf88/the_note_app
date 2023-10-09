5f86ff9dd0ddecc8e4e7158e49d996dcf7d1e20e
Fahed Nassar <engr.f.nassar@gmail.com>
2023-10-09 17:08:24 +0300

    Added welcome and home pages

--- app.py
+++ app.py
@@ -7,5 +7,23 @@ app = Flask("The Note App")
 
 @app.route("/")
 def index():
-    test = "Hello User"
-    return render_template("/login.html", test=test)
+    if request.method == "GET":
+        name = "METHOD IS GET"
+    else:
+        name = "METHOD IS POST"
+
+    return render_template("/index.html", name=name)
+
+
+@app.route("/login")
+def login():
+    return render_template("/login.html")
+
+
+@app.route("/welcome", methods=["GET", "POST"])
+def welcome():
+    name = "Fahed"
+    return render_template(
+        "/welcome.html",
+        name=name,
+    ), {"Refresh": "2; url=/"}

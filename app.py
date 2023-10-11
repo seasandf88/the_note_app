0357997d17dd6c966e6f09d97cad98be231c2e5d
Fahed Nassar <engr.f.nassar@gmail.com>
2023-10-11 13:43:40 +0300

    Add notes and clean the code

--- app.py
+++ app.py
@@ -2,42 +2,48 @@ import datetime, json, random
 
 from flask import Flask, redirect, render_template, request, session
 
-
 app = Flask(__name__)
 
+# required secret key for the session functionality
 app.secret_key = "hello"
-# app.permanent_session_lifetime = datetime.timedelta(days=1)
 
 
 @app.route("/", methods=["GET"])
 def index():
     if "user" in session:
-        username = session["user"]
+        # reads the users.json file and puts the list inside users variable
         users = read_json()
         for user in users:
-            if user["name"] == username:
-                return render_template("/index.html", user_name=user["name"], user_notes=user["notes"])
+            # match the logged in user with the existing users
+            if user["name"] == session["user"]:
+                # if the user exists redirect to homepage
+                return render_template(
+                    "/index.html", user_name=user["name"], user_notes=user["notes"]
+                )
     else:
+        # if it's a new user, redirect to login 
         return redirect("/login")
-
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        username = request.form.get("user-name").lower()
         users = read_json()
-        session["user"] = username
-        # session.permanent = True
+        # store a lower cased user name inside session["user"]
+        session["user"] = request.form.get("user-name").lower() 
+        # if there are no users, redirect to /new-user
         if len(users) == 0:
             return redirect("/new-user")
         else:
             for user in users:
-                if username == user["name"]:
+                if session["user"] == user["name"]:
+                    # if the user exists, redirect to /welcome
                     return redirect("/welcome")
-                
-            return redirect("/new-user")  
+
+            # if the user doesn't exist redirect to create an new user
+            return redirect("/new-user")
     else:
+        # just render the page if the request method id GET
         return render_template("/login.html")
 
 
@@ -46,86 +52,104 @@ def welcome():
     return render_template(
         "/welcome.html",
         user_name=session["user"],
-    ), {"Refresh": "2; url=/"}
+    ), {"Refresh": "2; url=/"} # redirect to homepage after 2 seconds
 
 
 @app.route("/logout")
 def logout():
-    session.clear()
+    session.clear() # clear the session storage then redirect to login page
     return redirect("/login")
 
 
-@app.route("/new-user")
+@app.route("/new-user") 
 def new_user():
-    username = session["user"]
-    new_user = create_user(username)
+    new_user = create_user(session["user"]) # creates new user dictionary
 
-    users = read_json()
-    users.append(new_user)
+    users = read_json() # put the users inside an array
+    users.append(new_user) # append the new user to the array 
 
-    write_json(users)
-    return redirect('/welcome')
+    write_json(users) # write the new array to file
+    return redirect("/welcome")
 
 
 @app.route("/new-note", methods=["POST"])
 def new_note():
-    username = session["user"]
-    content = request.form.get("note")
-    users = read_json()
-    for user in users:
-        if (user["name"] == username):
-            number_of_notes = len(user["notes"])
-            note = create_note(number_of_notes, content)
-            user["notes"].append(note)
-
-    write_json(users)
-    return redirect("/")
-            
-
-@app.route("/delete-note", methods=["GET", "POST"])
-def delete():
-    note_id = int(request.form.get("note-id"))
+    content = request.form.get("note") # get the note content from the form
     users = read_json()
     for user in users:
         if user["name"] == session["user"]:
-            filtered_notes = list(filter(lambda n: n["id"] != note_id, user["notes"]))
+            # get the number of notes for the current user
+            number_of_notes = len(user["notes"])
+            # creates a new note with the index and content as arguments
+            note = create_note(number_of_notes, content)
+            # append the new note to the user's notes array
+            user["notes"].append(note)
+
+    write_json(users) # rewrite the json file
+    return redirect("/")
+
+
+@app.route("/delete-note", methods=["POST"])
+def delete():
+    note_id = int(request.form.get("note-id")) # store the note index
+    users = read_json()
+    for user in users:
+        if user["name"] == session["user"]:
+            # filtering out the note that match the id
+            filtered_notes = list(filter(lambda note: note["id"] != note_id, user["notes"]))
+            # update the user notes array
             user["notes"] = filtered_notes
-            
+
     write_json(users)
 
-    return redirect('/')
-
+    return redirect("/")
 
 
 @app.route("/results")
 def results():
+    # get the search query from the form search field
     query = request.args.get("search").lower()
     users = read_json()
-    results = []
+    results = [] # empty results array
     for user in users:
-        if user["name"] == session["user"]:
-            for note in user["notes"]:
+        if user["name"] == session["user"]: # match the user name
+            for note in user["notes"]: # loop through user notes
+                # this evaluate to true if a match was found
                 if note["content"].lower().find(query) != -1:
+                    # append the matched note to the results array
                     results.append(note)
-            return render_template("/results.html", user_name=user["name"], results=results)
+            return render_template(
+                "/results.html", user_name=user["name"], results=results
+            )
 
 
+# creates a dict for the new user
 def create_user(name):
     return {"name": name, "notes": []}
 
+
+# create a dict for the new note
 def create_note(id, content):
     date = datetime.datetime.now()
+    # generates a random number between 1 and 7
     rand = random.randint(1, 7)
     color = "accent-" + str(rand)
-    return {"id": id, "timestamp": date.strftime("%d %b %y %H:%M"), "content": content, "color": color}
+    return {
+        "id": id,
+        "timestamp": date.strftime("%d %b %y %H:%M"),
+        "content": content,
+        "color": color,
+    }
 
 
+# reads and returns the users.json content as an array
 def read_json():
     with open("./users.json", "r") as file:
         file_content = json.load(file)
     return file_content
 
 
+# writes the passed array to the users.json file
 def write_json(data):
     with open("./users.json", "w") as file:
-        file.write(json.dumps(data, indent = 2))
+        file.write(json.dumps(data, indent=2))
--- static/script.js
+++ static/script.js
@@ -1,7 +1,10 @@
+// Getting the dialog elements from the DOM
 const addNoteBtn = document.getElementById("add-note")
 const noteDialog = document.getElementById("note-dialog")
 const formCancel = document.getElementById("form-cancel")
 
+// Open the Dialog using a built-in method showModal()
 addNoteBtn.addEventListener("click", () => noteDialog.showModal())
 
+// Close the Dialog using a built-in method close()
 formCancel.addEventListener("click", () => noteDialog.close())
